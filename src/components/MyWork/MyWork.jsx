6e4ab3e2bb0f526550a1c0cf035cf02418d4883c
Michael Chege <mikechege770@gmail.com>
2024-09-21 22:43:54 +0300

    dadda

--- src/components/MyWork/MyWork.jsx
+++ src/components/MyWork/MyWork.jsx
@@ -28,7 +28,7 @@ const MyWork = () => {
       <div id="work" className="mywork fade-in">
         <motion.div 
          whileInView={{ opacity: 1, x: 0 }}
-         initial={{ opacity: 0, x: -100 }}
+         initial={{ opacity: 0, x: 100 }}
          transition={{ duration: 1.5 }}
 
         className="mywork-container">
